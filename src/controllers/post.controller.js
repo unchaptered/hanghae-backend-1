2209a08123f462e141444d387fdd9e70a44a3eeb
Unchaptered <workstation19961002@gmail.com>
2022-07-24 15:55:13 +0900

    Refactor `**/post.controller.js`, [ISSUE] 비동기 코드 에러 핸들 필요 반영
    https://github.com/unchaptered/hanghae-backend-1/issues/2

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,98 +2,207 @@ import { postModel } from '../schemas/_.loader.js';
 
 export async function getPostsByQuery(req, res) {
 
-    const { sort } = req.query;
+    try {
 
-    const result = await postModel.find({})
-                                .sort({ 'createdAt': sort === 'asc' ? 'asc' : 'desc' })
-                                .select('title owner createdAt');
-    if (result === null)
-        return res.status(201).json({
+        const { sort } = req.query;
+
+        const result = await postModel.find({})
+                                    .sort({ 'createdAt': sort === 'asc' ? 'asc' : 'desc' })
+                                    .select('title owner createdAt');
+        if (result === null)
+            return res.status(404).json({
+                isSuccess: false,
+                message: '게시글이 존재하지 않습니다.',
+                result: {}
+            });
+        else
+            return res.json({
+                isSuccess: true,
+                message: '게시글 불러오기에 성공하였습니다.',
+                result: result.map(v => ({
+                    _id: v._id,
+                    title: v.title,
+                    owner: v.owner,
+                    createdAt: v.createdAt
+                }))
+            });
+
+    } catch(err) {
+
+        return res.status(500).json({
             isSuccess: false,
-            message: '게시글이 존재하지 않습니다.',
+            message: `${err.name} : ${err.message}`,
             result: {}
         });
-    else
-        return res.json({
-            isSuccess: true,
-            message: '게시글 불러오기에 성공하였습니다.',
-            result: result.map(v => ({
-                _id: v._id,
-                title: v.title,
-                owner: v.owner,
-                createdAt: v.createdAt
-            }))
-        });
+
+    }
     
 }
 
 export async function createPost(req, res) {
 
-    const { title, context, owner, password } = req.body;
+    try {
 
-    if (title.length >=3 && title.length <= 30
-        && context.length >= 3 && context.length <= 300
-        && owner.length >= 1 && password.length >= 1) {
+        const { title, context, owner, password } = req.body;
 
-        const result = await new postModel({ title, context, owner, password }).save();
-        console.log(result);
-
-        return res.status(200).json({
-            message: 'success',
-            result
+        if (title.length >=3 && title.length <= 30
+            && context.length >= 3 && context.length <= 300
+            && owner.length >= 1 && password.length >= 1) {
+    
+            const result = await new postModel({ title, context, owner, password }).save();
+    
+            return res.status(200).json({
+                isSuccess: true,
+                message: '게시글 작성이 완료되었습니다.',
+                result
+            });
+    
+        } else return res.status(400).json({
+            isSuccess: false,
+            message: '잘못된 요청이 전달되었습니다. 변수의 범위를 고려해서 보내주세요',
+            result: {}
         });
 
-    } else return res.status(400).json('hello');
+    } catch(err) {
+
+        return res.status(500).json({
+            isSuccess: false,
+            message: `${err.name} : ${err.message}`,
+            result: {}
+        });
+
+    }
+    
 
 }
 
 export async function getPostById(req, res) {
 
-    const { id } = req.params;
+    try {
 
-    return res.status(200).json(await postModel.findById(id).select('title context owner'));
+        const { id } = req.params;
+
+        const result = await postModel.findById(id).select('title context owner')
+        if (result === null)
+            return res.status(201).json({
+                isSuccess: false,
+                message: '게시글이 존재하지 않습니다.',
+                result: {}
+            });
+        else
+            return res.status(201).json({
+                isSuccess: false,
+                message: '게시글 수정이 완료되었습니다.',
+            })
+
+    } catch(err) {
+
+        return res.status(500).json({
+            isSuccess: false,
+            message: `${err.name} : ${err.message}`,
+            result: {}
+        });
+
+    }
 
 }
 
 export async function putPostById(req, res) {
 
-    const {
-        params: { id },
-        body: { title, context, owner, password }
-    } = req;
+    try {
 
-    if (title?.length >=3 && title?.length <= 30
-        && context?.length >= 3 && context?.length <= 300) {
+        const {
+            params: { id },
+            body: { title, context, owner, password }
+        } = req;
+    
+        if (title?.length >=3 && title?.length <= 30
+            && context?.length >= 3 && context?.length <= 300) {
+    
+            const isExists = await postModel.exists({ _id: id });
+            if (isExists === null)
+                return res.status(404).json({
+                    isSuccess: false,
+                    message: '존재하지 않는 사용자입니다.',
+                    result: {}
+                });
+    
+            const result = await postModel.findOneAndUpdate({ $and: [{ _id: id }, { owner }, { password }]}, { title, context }).select('title context owner password');
+            if (result === null)
+                return res.status(404).json({
+                    isSuccess: false,
+                    message: '소유주가 아닌 사용자입니다.',
+                    result: {}
+                });
+            else
+                return res.status(201).json({
+                    isSuccess: false,
+                    message: '수정에 성공하셨습니다.',
+                    result: {}
+                });
+    
+        } else return res.status(400).json({
+            isSuccess: false,
+            message: '잘못된 요청이 전달되었습니다. 변수의 범위를 고려해서 보내주세요',
+            result: {}
+        });
 
-        const isExists = await postModel.exists({ _id: id });
-        if (isExists === null) return res.status(404).json('존재하지 않는 사용자입니다.');
+    } catch(err) {
 
-        const result = await postModel.findOneAndUpdate({ $and: [{ _id: id }, { owner }, { password }]}, { title, context }).select('title context owner password');
-        if (result === null) return res.status(403).json('소유주가 아닌 사용자입니다.');
+        return res.status(500).json({
+            isSuccess: false,
+            message: `${err.name} : ${err.message}`,
+            result: {}
+        });
 
-        return res.status(201).json('수정에 성공하셨습니다.');
-
-    } else return res.status(400).json('파라미터 누락');
+    }
+    
     
 }
 
 export async function deletePostById(req, res) {
 
-    const {
-        params: { id },
-        body: { owner, password }
-    } = req;
-    
-    if (owner.length >= 1 && password.length >= 1) {
+    try {
 
-        const isExists = await postModel.exists({ _id: id });
-        if (isExists === null) return res.status(404).json('존재하지 않는 사용자입니다.');
+        const {
+            params: { id },
+            body: { owner, password }
+        } = req;
+        
+        if (owner.length >= 1 && password.length >= 1) {
     
-        const result = await postModel.findOneAndDelete({ $and: [{ _id: id }, { owner }, { password }]});
-        if (result === null) return res.status(403).json('소유주가 아닌 사용자입니다.');
+            const isExists = await postModel.exists({ _id: id });
+            if (isExists === null)
+                return res.status(404).json({
+                    isSuccess: false,
+                    message: '존재하지 않는 사용자입니다.',
+                    result: {}
+                });
+        
+            const result = await postModel.findOneAndDelete({ $and: [{ _id: id }, { owner }, { password }]});
+            if (result === null)
+                return res.status(404).json({
+                    isSuccess: false,
+                    message: '소유주가 아닌 사용자입니다.',
+                    result: {}
+                });
+        
+            return res.status(201).json('삭제에 성공하였습니다.');
     
-        return res.status(201).json('삭제에 성공하였습니다.');
+        } else return res.status(400).json({
+            isSuccess: false,
+            message: '잘못된 요청이 전달되었습니다. 변수의 범위를 고려해서 보내주세요',
+            result: {}
+        });
+        
+    } catch(err) {
 
-    } else return res.status(200).json('파라미터 누락');
+        return res.status(500).json({
+            isSuccess: false,
+            message: `${err.name} : ${err.message}`,
+            result: {}
+        });
+
+    }
 
 }
