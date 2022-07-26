644d9ffe38ab388bc7da21feb0d99bbb0a5a861a
Unchaptered <workstation19961002@gmail.com>
2022-07-26 15:25:59 +0900

    Update `**/post.(controller|services).js`, applying customException

--- src/layers/controllers/post.controller.js
+++ src/layers/controllers/post.controller.js
@@ -9,23 +9,16 @@ export async function getPostsByQuery(req, res) {
 
         const result = await postService.getPostsByQuery(sort);
 
-        if (result === null)
-            return res.status(404).json({
-                isSuccess: false,
-                message: '게시글이 존재하지 않습니다.',
-                result: {}
-            });
-        else
-            return res.json({
-                isSuccess: true,
-                message: '게시글 불러오기에 성공하였습니다.',
-                result: result.map(v => ({
-                    _id: v._id,
-                    title: v.title,
-                    owner: v.owner,
-                    createdAt: v.createdAt
-                }))
-            });
+        return res.json({
+            isSuccess: true,
+            message: '게시글 불러오기에 성공하였습니다.',
+            result: result.map(v => ({
+                _id: v._id,
+                title: v.title,
+                owner: v.owner,
+                createdAt: v.createdAt
+            }))
+        });
 
     } catch(err) {
 
@@ -76,18 +69,10 @@ export async function getPostById(req, res) {
         const { id } = req.params;
 
         const result = await postService.getPostById(id);
-
-        if (result === null)
-            return res.status(201).json({
-                isSuccess: false,
-                message: '게시글이 존재하지 않습니다.',
-                result: {}
-            });
-        else
-            return res.status(201).json({
-                isSuccess: false,
-                message: '게시글 수정이 완료되었습니다.',
-            })
+        return res.status(201).json({
+            isSuccess: false,
+            message: '게시글 수정이 완료되었습니다.',
+        })
 
     } catch(err) {
 
@@ -109,14 +94,6 @@ export async function putPostById(req, res) {
     
         if (title?.length >=3 && title?.length <= 30
             && context?.length >= 3 && context?.length <= 300) {
-    
-            // const isExists = await postModel.exists({ _id: id });
-            // if (isExists === null)
-            //     return res.status(404).json({
-            //         isSuccess: false,
-            //         message: '존재하지 않는 사용자입니다.',
-            //         result: {}
-            //     });
     
             const result = await postService.putPostById(id, title, context, owner, passowrd);
 
@@ -160,24 +137,13 @@ export async function deletePostById(req, res) {
         
         if (owner.length >= 1 && password.length >= 1) {
     
-            // const isExists = await postModel.exists({ _id: id });
-            // if (isExists === null)
-            //     return res.status(404).json({
-            //         isSuccess: false,
-            //         message: '존재하지 않는 사용자입니다.',
-            //         result: {}
-            //     });
-        
             const result = await postService.deletePostById(id, owner, passowrd);
-
-            if (result === null)
-                return res.status(404).json({
-                    isSuccess: false,
-                    message: '소유주가 아닌 사용자입니다.',
-                    result: {}
-                });
         
-            return res.status(201).json('삭제에 성공하였습니다.');
+            return res.status(201).json({
+                isSuccess: true,
+                message: '게시글이 삭제되었습니다.',
+                result: result
+            });
     
         } else return res.status(400).json({
             isSuccess: false,
--- src/layers/services/post.service.js
+++ src/layers/services/post.service.js
@@ -1,12 +1,32 @@
+import { isExistsTypeAnnotation } from "@babel/types";
+import { NotFoundException, UnauthorizedException } from "../../models/exception/custom.exception";
+
+/**
+ * 
+ * @param {*} sort 'asc' | 'desc'
+ * @returns `Object` : { title, owner, createdAt }
+ * @throws `NotFoundException`
+ */
 export async function getPostsByQuery(sort) {
 
     const result = await postModel.find({})
         .sort({ 'createdAt': sort === 'asc' ? 'asc' : 'desc' })
         .select('title owner createdAt');
 
+    if (result === null)
+        throw NotFoundException('게시글이 존재하지 않습니다.');
+
     return result;
 };
 
+/**
+ * 
+ * @param {*} title 
+ * @param {*} context 
+ * @param {*} owner 
+ * @param {*} password 
+ * @returns `Object` : { _id, title, context, owner, password }
+ */
 export async function createPost(title, context, owner, password) {
 
     const result = await new postModel({ title, context, owner, password }).save();
@@ -15,26 +35,69 @@ export async function createPost(title, context, owner, password) {
 
 };
 
+/**
+ * 
+ * @param {*} id 
+ * @returns `Object` : { title, context, owner }
+ * @thorws `NotFoundException`
+ */
 export async function getPostById(id) {
 
     const result = await postModel.findById(id)
                                     .select('title context owner')
+    if (result === null)
+        throw new NotFoundException('게시글이 존재하지 않습니다.');
 
     return result;
 
 };
 
+/**
+ * 
+ * @param {*} id 
+ * @param {*} title 
+ * @param {*} context 
+ * @param {*} owner 
+ * @param {*} password 
+ * @returns Object : { title, context, owner, password }
+ * @throws `NotFoundException`
+ * @throws `UnauthorizedException`
+ */
 export async function putPostById(id, title, context, owner, password) {
 
-    const result = await postModel.findOneAndUpdate({ $and: [{ _id: id }, { owner }, { password }]}, { title, context }).select('title context owner password');
-    
+    const isExists = await postModel.exists({ _id: id });
+    if (isExists === null)
+        throw new NotFoundException('존재하지 않는 사용자 입니다.');
+
+    const result = await postModel.findOneAndUpdate({ $and: [{ _id: id }, { owner }, { password }]}, { title, context })
+                                    .select('title context owner password');
+    if (result === null)
+        throw new UnauthorizedException('소유주가 아닌 사용자입니다.');
+
     return result;
 
 };
 
+/**
+ * 
+ * @param {*} id 
+ * @param {*} owner 
+ * @param {*} password 
+ * @returns Object : { title, context, owner, password }
+ * @throws `NotFoundException`
+ * @throws `UnauthorizedException`
+ */
 export async function deletePostById(id, owner, password) {
 
-    const result = await postModel.findOneAndDelete({ $and: [{ _id: id }, { owner }, { password }]});
+    const isExists = await postModel.exists({ _id: id });
+    if (isExists === null)
+        throw new NotFoundException('존재하지 않는 사용자 입니다.');
+
+    const result = await postModel.findOneAndDelete({ $and: [{ _id: id }, { owner }, { password }]})
+                                    .select('title context owner password');
+
+    if (result === null)
+        throw new UnauthorizedException('소유주가 아닌 사용자 입니다.');
     
     return result;
 
