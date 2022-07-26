a14a82fb962acde655b021ef3610d009f558a017
Unchaptered <workstation19961002@gmail.com>
2022-07-26 15:57:34 +0900

    Add response class `Form`, `SuccessForm`, `FailureForm`

--- src/models/_.loader.js
+++ src/models/_.loader.js
@@ -2,13 +2,18 @@ import { postModel } from './schemas/post.js';
 import { commentModel } from './schemas/comment.js';
 
 import { CustomException, BadRequestException, UnauthorizedException, NotFoundException } from './exception/custom.exception.js';
-
+import { Form, SuccessForm, FailureForm } from './form/form.js';
 
 export {
 
     // Schema
     postModel,
     commentModel,
+
+    // Form
+    Form,
+    SuccessForm,
+    FailureForm,
 
     // Exception
     CustomException,
