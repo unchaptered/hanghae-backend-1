30f860d56220a0636e81e8eb3e951f5f63589028
Unchaptered <workstation19961002@gmail.com>
2022-07-27 20:15:22 +0900

    Update `**/modules/_.loader.js`, 글로벌 예외 미들웨어 추가

--- src/modules/_.lodaer.js
+++ src/modules/_.lodaer.js
@@ -1,11 +1,17 @@
 import { errorHandler } from './handler/error.handler.js';
 import { exceptionHandler } from './handler/exception.handler.js';
+import { exceptionMiddleware } from './middlewares/exception.middleware.js';
 
 import { FormFactory } from './factories/form.factory.js';
 
 
 export {
+
     errorHandler,
+
     FormFactory,
-    exceptionHandler
+
+    exceptionHandler,
+    exceptionMiddleware
+    
 }
